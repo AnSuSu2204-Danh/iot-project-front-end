b08f65ce6258021d503dc701d13ca99c28500bcf
Danh Nguyen <ncdanh2204@gmail.com>
2024-06-10 11:09:43 +0700

    update

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useHistory } from 'react-router-dom';
-import './SignIn.css'; // File CSS cho trang đăng nhập
+import './SignIn.css';
 
 const SignIn = () => {
     const [email, setEmail] = useState('');
@@ -25,53 +25,44 @@ const SignIn = () => {
     };
 
     return (
-        <div className="container-scroller">
-            <div className="container-fluid page-body-wrapper full-page-wrapper">
-                <div className="row w-100 m-0">
-                    <div className="content-wrapper full-page-wrapper d-flex align-items-center auth login-bg">
-                        <div className="card col-lg-4 mx-auto">
-                            <div className="card-body px-5 py-5">
-                                <h3 className="card-title text-left mb-3">Sign In</h3>
-                                <form onSubmit={handleSubmit}>
-                                    <div className="form-group">
-                                        <label>Email *</label>
-                                        <input 
-                                            type="email" 
-                                            className="form-control p_input" 
-                                            value={email}
-                                            onChange={(e) => setEmail(e.target.value)}
-                                            required
-                                        />
-                                    </div>
-                                    <div className="form-group">
-                                        <label>Password *</label>
-                                        <input 
-                                            type="password" 
-                                            className="form-control p_input" 
-                                            value={password}
-                                            onChange={(e) => setPassword(e.target.value)}
-                                            required
-                                        />
-                                    </div>
-                                    <div className="form-group d-flex align-items-center justify-content-between">
-                                        <div className="form-check">
-                                            <label className="form-check-label">
-                                                <input type="checkbox" className="form-check-input" /> Remember me </label>
-                                        </div>
-                                    </div>
-                                    <div className="text-center">
-                                        <button type="submit" className="btn btn-primary btn-block enter-btn">Sign In</button>
-                                    </div>
-                                    <div className="d-flex">
-                                        <button className="btn btn-facebook col mr-2">
-                                            <i className="mdi mdi-facebook"></i> Facebook </button>
-                                        <button className="btn btn-google col">
-                                            <i className="mdi mdi-google-plus"></i> Google plus </button>
-                                    </div>
-                                    {error && <div className="alert alert-danger mt-3">{error}</div>}
-                                    <p className="sign-up">Don't have an Account?<a href="/signup"> Sign Up</a></p>
-                                </form>
-                            </div>
+        <div className="container">
+            <div className="row justify-content-center">
+                <div className="col-md-6">
+                    <div className="card">
+                        <div className="card-body">
+                            <h3 className="card-title text-center">Sign In</h3>
+                            <form onSubmit={handleSubmit}>
+                                <div className="form-group">
+                                    <label htmlFor="email">Email</label>
+                                    <input 
+                                        type="email" 
+                                        className="form-control" 
+                                        value={email}
+                                        onChange={(e) => setEmail(e.target.value)}
+                                        required
+                                    />
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="password">Password</label>
+                                    <input 
+                                        type="password" 
+                                        className="form-control" 
+                                        value={password}
+                                        onChange={(e) => setPassword(e.target.value)}
+                                        required
+                                    />
+                                </div>
+                                <div className="form-group form-check">
+                                    <input 
+                                        type="checkbox" 
+                                        className="form-check-input" 
+                                        id="rememberMe"
+                                    />
+                                    <label className="form-check-label" htmlFor="rememberMe">Remember me</label>
+                                </div>
+                                <button type="submit" className="btn btn-primary btn-block">Sign In</button>
+                            </form>
+                            {error && <div className="alert alert-danger mt-3">{error}</div>}
                         </div>
                     </div>
                 </div>
--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import SignIn from './components/SignIn';
+import Dashboard from './components/Dashboard';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const App = () => (
+    <Router>
+        <Switch>
+            <Route exact path="/" component={SignIn} />
+            <Route path="/dashboard" component={Dashboard} />
+        </Switch>
+    </Router>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<App />, document.getElementById('root'));
